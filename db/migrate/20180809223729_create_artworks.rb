45c8f85b57e431881e00b95215fab179a8f8c711
Guy Edelstyn <guy@h21.uk>
2018-08-09 23:40:39 +0100

    Created Model

---
+++ db/migrate/20180809223729_create_artworks.rb
@@ -0,0 +1,20 @@
+class CreateArtworks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artworks do |t|
+      t.string :title
+      t.date :year
+      t.string :medium
+      t.integer :price
+      t.text :description
+      t.string :collection
+      t.string :location
+      t.integer :height
+      t.integer :width
+      t.integer :depth
+      t.boolean :sold
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
