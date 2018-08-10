a7688a243eb9d40dc81ada3e0ed5fcc53b07ba5e
danielcampos <ddregalo@gmail.com>
2018-08-10 21:09:19 +0100

    Add create artwork post

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,10 @@
 class ArtworksController < ApplicationController
   def index
+    @artworks = Artwork.all
+  end
+
+  def create
+    @artwork = artworks.create(artwork_params)
+    redirect_to artworks_path
   end
 end
