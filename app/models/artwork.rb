041dde44286b94811ddc12381ea9c8e9e79cf21f
Guy Edelstyn <guy@h21.uk>
2018-09-12 22:10:37 +0100

    : infrot of types

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -4,20 +4,20 @@ class Artwork < ApplicationRecord
 
   settings do
     mappings dynamic: false do
-      indexes :title, type: text, analyzer: :english
-      indexes :year, type: date
-      indexes :medium, type: text, analyzer: :english
-      indexes :price, type: number
-      indexes :description, text, analyzer: :english
-      indexes :collection, type: text, analyzer: :english
-      indexes :location, type: text, analyzer: :english
-      indexes :height, type: number
-      indexes :width, type: number
-      indexes :depth, type: number
-      indexes :sold, type: boolean
-#     indexes :notes, type: text, analyzer: :english
-      indexes :created_at, type: date
-      indexes :updated_at, type: date
+      indexes :title, type: :text, analyzer: :english
+      indexes :year, type: :date
+      indexes :medium, type: :text, analyzer: :english
+      indexes :price, type: :number
+      indexes :description, :text, analyzer: :english
+      indexes :collection, type: :text, analyzer: :english
+      indexes :location, type: :text, analyzer: :english
+      indexes :height, type: :number
+      indexes :width, type: :number
+      indexes :depth, type: :number
+      indexes :sold, type: :boolean
+#     indexes :notes, type: :text, analyzer: :english
+      indexes :created_at, type: :date
+      indexes :updated_at, type: :date
     end
   end
 end
