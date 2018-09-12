1450ff20694b9f438f343c4c9830b26fee1e5cb6
Guy Edelstyn <guy@h21.uk>
2018-09-12 22:06:53 +0100

    update elasticsearch schema

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,30 @@
 class Artwork < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings do
+    mappings dynamic: false do
+      indexes :title, type: text, analyzer: :english
+      indexes :year, type: date
+      indexes :medium, type: text, analyzer: :english
+      indexes :price, type: number
+      indexes :description, text, analyzer: :english
+      indexes :collection, type: text, analyzer: :english
+      indexes :location, type: text, analyzer: :english
+      indexes :height, type: number
+      indexes :width, type: number
+      indexes :depth, type: number
+      indexes :sold, type: boolean
+#     indexes :notes, type: text, analyzer: :english
+      indexes :created_at, type: date
+      indexes :updated_at, type: date
+    end
+
+
+
+
+
+
+    end
+  end
 end
