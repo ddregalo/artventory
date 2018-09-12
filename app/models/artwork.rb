06544ee2451c5f9d95daeee4075efe1666886649
Guy Edelstyn <guy@h21.uk>
2018-09-12 22:38:15 +0100

    correct type errors

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -7,15 +7,14 @@ class Artwork < ApplicationRecord
       indexes :title, type: :text, analyzer: :english
       indexes :year, type: :date
       indexes :medium, type: :text, analyzer: :english
-      indexes :price, type: :number
-      indexes :description, :text, analyzer: :english
+      indexes :price, type: :float
+      indexes :description, type: :text, analyzer: :english
       indexes :collection, type: :text, analyzer: :english
       indexes :location, type: :text, analyzer: :english
-      indexes :height, type: :number
-      indexes :width, type: :number
-      indexes :depth, type: :number
+      indexes :height, type: :integer
+      indexes :width, type: :integer
+      indexes :depth, type: :integer
       indexes :sold, type: :boolean
-#     indexes :notes, type: :text, analyzer: :english
       indexes :created_at, type: :date
       indexes :updated_at, type: :date
     end
