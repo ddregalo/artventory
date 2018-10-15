e1dafccc947c5e9e91136189bb10833f39030a17
Daniel <daniel@g-mac-superpro.local>
2018-10-15 20:26:11 +0100

    Add completed month to artworks schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_09_201031) do
+ActiveRecord::Schema.define(version: 2018_10_15_192212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_09_09_201031) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.integer "completed_month"
   end
 
   create_table "users", force: :cascade do |t|
