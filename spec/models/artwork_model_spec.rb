b26915bda95a47ceeb6865c54160e53632e69c51
danielcampos <ddregalo@gmail.com>
2018-11-20 22:53:51 +0000

    Add test search by description

--- spec/models/artwork_model_spec.rb
+++ spec/models/artwork_model_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Artwork, elasticsearch: true, type: :model do
-  it 'artwork should be indexed' do
+  it 'artwork should be indexed search by title' do
     Artwork.create(
       title: 'Artwork 1'
     )
@@ -11,5 +11,18 @@ RSpec.describe Artwork, elasticsearch: true, type: :model do
 
     # verify your model was indexed
     expect(Artwork.search('Artwork 1').records.length).to eq(1)
+  end
+
+  it 'artwork should be indexed search by description' do
+    Artwork.create(
+      title: 'Artwork 1',
+      description: 'Artwork 1 description'
+    )
+
+    # refresh the index
+    Artwork.__elasticsearch__.refresh_index!
+
+    # verify your model was indexed
+    expect(Artwork.search('Artwork 1 description').records.length).to eq(1)
   end
 end
