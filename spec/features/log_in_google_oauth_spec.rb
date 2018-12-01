898fa7a04810364df6a406e2d9fa382312580a92
danielcampos <ddregalo@gmail.com>
2018-12-01 14:46:39 +0000

    Test invalid google login

--- spec/features/log_in_google_oauth_spec.rb
+++ spec/features/log_in_google_oauth_spec.rb
@@ -20,14 +20,14 @@ RSpec.describe 'Log In with Google OAuth', type: :feature do
     expect(page).to have_content('Artventory V0.0.0.0.0.2a')
   end
 
-  # scenario 'Sign in with google failure' do
-  #   # Turn on 'test mode' for Omniauth
-  #   OmniAuth.config.test_mode = true
-  #   # Mock google authentification hash with invalid credentials
-  #   OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
-  #   sign_in_with_google_oauth
-  #   expect(page).to have_content('Could not authenticate you from GoogleOauth2')
-  # end
+  scenario 'Sign in with google failure' do
+    # Turn on 'test mode' for Omniauth
+    OmniAuth.config.test_mode = true
+    # Mock google authentification hash with invalid credentials
+    OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+    sign_in_with_google_oauth
+    expect(page).to have_content('Could not authenticate you from GoogleOauth2')
+  end
 
   # scenario 'Sign in with existing google account' do
   #   google_authenticate
