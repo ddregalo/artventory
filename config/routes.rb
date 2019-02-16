aaabb8a96d3a02900b3d412387b98baaef7889c1
danielcampos <ddregalo@gmail.com>
2019-02-16 22:31:03 +0000

    Add home controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :user, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  root 'home#index'
   get 'artworks/index'
-  root 'artworks#index'
   get 'artworks/new'
   get 'artworks/edit'
   get 'artworks/delete'
