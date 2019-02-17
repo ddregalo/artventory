a054faf61ea8f2946c1a0062b1fd69abe7a88dae
danielcampos <ddregalo@gmail.com>
2019-02-17 22:10:02 +0000

    Add avatar column migration to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_093148) do
+ActiveRecord::Schema.define(version: 2019_02_17_220719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
