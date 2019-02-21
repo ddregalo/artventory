7c495ca4b3d5f1923a9a6a8c423bb69652e67e08
danielcampos <ddregalo@gmail.com>
2019-02-21 22:16:51 +0000

    Rubocop linting fixes + update migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,31 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer
-      .permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
-    
+      .permit(:sign_up) { |u|
+        u.permit(
+          :username,
+          :email,
+          :password,
+          :password_confirmation,
+          :remember_me,
+          :avatar,
+          :avatar_cache,
+          :remove_avatar
+        )
+      }
+
     devise_parameter_sanitizer
-      .permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
+      .permit(:account_update) { |u|
+        u.permit(
+          :username,
+          :email,
+          :password,
+          :password_confirmation,
+          :current_password,
+          :avatar,
+          :avatar_cache,
+          :remove_avatar
+        )
+      }
   end
 end
