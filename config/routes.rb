7483009c03a43ebcc7ba75cc578fdf1f6a5318b4
danielcampos <ddregalo@gmail.com>
2019-02-23 18:52:11 +0000

    Add users to resources and get home/index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@
 Rails.application.routes.draw do
   devise_for :user, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root 'home#index'
+  get 'home/index'
   get 'artworks/index'
   get 'artworks/new'
   get 'artworks/edit'
   get 'artworks/delete'
+  resources :users
   resources :artworks do
     collection do
       get :search
