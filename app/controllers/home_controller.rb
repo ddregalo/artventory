b3f02b7d4007d4806ced23a2e6700b6a091169ba
danielcampos <ddregalo@gmail.com>
2019-02-23 18:53:07 +0000

    Set user uid if not set on login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+require 'securerandom'
+
 class HomeController < ApplicationController
-  def Index
+  def index
+    p current_user.uid
+    if !current_user.uid.present?
+      current_user.update(uid: SecureRandom.uuid)
+      p current_user.uid
+    end
   end
 end
