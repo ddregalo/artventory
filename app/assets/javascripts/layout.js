45ee88aaf71b6261d7bf1d4cce5d71f9a32c6930
danielcampos <ddregalo@gmail.com>
2019-02-24 23:44:23 +0000

    Customize sidebar - close inside of sidebar and hamburger outside

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,6 +1,17 @@
-$(document).ready(function () {
+ready = function() {
+    $('#hamburgerMenu').hide();
+        
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
+        $('#hamburgerMenu').toggleClass('active').fadeIn(800);
     });
-});
+    
+    $('#hamburgerMenu').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');  
+        $(this).toggleClass('active').fadeOut(400);
+    });
+};
+
+$(document).on('turbolinks:load', ready);
