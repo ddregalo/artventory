ea360cca019e5a0647b050e28633c3f04761f771
gedelstyn <guy@h21.uk>
2019-03-02 22:30:54 +0000

    Tidying up my mess

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -6,26 +6,18 @@ class PdfController < ApplicationController
   def generate_pdf
     Prawn::Document.generate(current_user.username + Time.new.to_s + ".pdf") do |pdf|
       pdf.formatted_text [ { :text => "A portfolio example\n\n", :size => 20} ]
-
       artworks = Artwork.where(uid: current_user.uid)
       
       artworks.each do |artwork|
-        
         pdf.bounding_box([50, pdf.cursor], :width => 400, :height => 350) do
           pdf.stroke_bounds
           pdf.formatted_text [ { :text => "Title: " + artwork.title, :size => 16 } ]
           pdf.formatted_text [ { :text => "Medium: " + artwork.medium, :size => 12 } ]
           pdf.formatted_text [ { :text => "Desc: " + artwork.description, :size => 12 } ]
-
-          pdf.image "/Users/guy/GitHub/artventory/public" + artwork.image.url, :width => 300, :position => :right
-
+          pdf.image "#{Rails.root}/public" + artwork.image.url, :width => 300, :position => :right
          end
-        
-        
         title_position = pdf.cursor
-
       end
-
     end
     redirect_to artworks_path
   end
