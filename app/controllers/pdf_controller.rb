c636eeff30732511e5cae8f44e1730737cc8c9fe
gedelstyn <guy@h21.uk>
2019-03-02 22:19:13 +0000

    Added visibility of artworks, started work on the first PDF generation

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,24 +1,32 @@
 class PdfController < ApplicationController
   def new
-
+    @artworks = Artwork.where(uid: current_user.uid)
   end
-
-  def create
-  end
-
 
   def generate_pdf
-    # Assignment
-    pdf = Prawn::Document.new
-    pdf.text "Hello World"
-    pdf.render_file "assignment.pdf"
-    # Implicit Block
-    Prawn::Document.generate("implicit.pdf") do
-    text "Hello World"
+    Prawn::Document.generate(current_user.username + Time.new.to_s + ".pdf") do |pdf|
+      pdf.formatted_text [ { :text => "A portfolio example\n\n", :size => 20} ]
+
+      artworks = Artwork.where(uid: current_user.uid)
+      
+      artworks.each do |artwork|
+        
+        pdf.bounding_box([50, pdf.cursor], :width => 400, :height => 350) do
+          pdf.stroke_bounds
+          pdf.formatted_text [ { :text => "Title: " + artwork.title, :size => 16 } ]
+          pdf.formatted_text [ { :text => "Medium: " + artwork.medium, :size => 12 } ]
+          pdf.formatted_text [ { :text => "Desc: " + artwork.description, :size => 12 } ]
+
+          pdf.image "/Users/guy/GitHub/artventory/public" + artwork.image.url, :width => 300, :position => :right
+
+         end
+        
+        
+        title_position = pdf.cursor
+
+      end
+
     end
-    # Explicit Block
-    Prawn::Document.generate("explicit.pdf") do |pdf|
-    pdf.text "Hello World"
-    end
+    redirect_to artworks_path
   end
 end
