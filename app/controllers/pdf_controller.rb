23a9e9826b9ec64afaed72dd35054a8540289c86
gedelstyn <guy@h21.uk>
2019-03-03 14:44:13 +0000

    Extract pdf creation to helper module

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,23 +1,12 @@
 class PdfController < ApplicationController
+  include PdfHelper
   def new
     @artworks = Artwork.where(uid: current_user.uid)
   end
 
   def generate_pdf
     Prawn::Document.generate(current_user.username + Time.new.to_s + ".pdf") do |pdf|
-      pdf.formatted_text [ { :text => "A portfolio example\n\n", :size => 20} ]
-      artworks = Artwork.where(uid: current_user.uid)
-      params[":ids"].each do |id|
-        artwork = artworks.find(id)
-        pdf.bounding_box([50, pdf.cursor], :width => 400, :height => 350) do
-          pdf.stroke_bounds
-          pdf.formatted_text [ { :text => "Title: " + artwork.title, :size => 16 } ]
-          pdf.formatted_text [ { :text => "Medium: " + artwork.medium, :size => 12 } ]
-          pdf.formatted_text [ { :text => "Desc: " + artwork.description, :size => 12 } ]
-          pdf.image "#{Rails.root}/public" + artwork.image.url, :width => 50, :position => :right
-        end
-        title_position = pdf.cursor
-      end
+      portfolio(pdf)
     end
     redirect_to artworks_path
   end
--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -1,2 +1,16 @@
 module PdfHelper
+    def portfolio(pdf)
+        pdf.formatted_text [ { :text => "A portfolio example\n\n", :size => 20} ]
+        artworks = Artwork.where(uid: current_user.uid)
+        params[":ids"].each do |id|
+          artwork = artworks.find(id)
+          pdf.bounding_box([50, pdf.cursor], :width => 400, :height => 350) do
+            pdf.stroke_bounds
+            pdf.formatted_text [ { :text => "Title: " + artwork.title, :size => 16 } ]
+            pdf.formatted_text [ { :text => "Medium: " + artwork.medium, :size => 12 } ]
+            pdf.formatted_text [ { :text => "Desc: " + artwork.description, :size => 12 } ]
+            pdf.image "#{Rails.root}/public" + artwork.image.url, :width => 50, :position => :right
+          end
+        end
+    end
 end
